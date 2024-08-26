e69df5e79ad6aebe4770b3012f2b74928f1e2669
Duc Huy Ly <78007468+duc-huy-ly@users.noreply.github.com>
2024-08-26 15:35:07 +0200

    solved ex3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(toReverse) {
+  let res = "";
+  for(let i = toReverse.length - 1; i >= 0 ; i--){
+    res += toReverse.charAt(i);
+  }
+  return res;
 };
 
 // Do not edit below this line
