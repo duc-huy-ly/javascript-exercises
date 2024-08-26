968e27fb4a665893fb9db8d145838d0c3e58b676
Duc Huy Ly <78007468+duc-huy-ly@users.noreply.github.com>
2024-08-26 15:22:19 +0200

    solved exercice 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(inputToRepeat, nbOfTimes) {
+  if(nbOfTimes< 0) return 'ERROR';
+  let res= '';
+  for(nbOfTimes; nbOfTimes > 0 ; nbOfTimes--){
+    res += inputToRepeat;
+  }
+  return res;
 };
 
 // Do not edit below this line
