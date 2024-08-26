e3ae9a2d71f8ed37a7d900d03d8e5e1b8d4ed46e
Duc Huy Ly <78007468+duc-huy-ly@users.noreply.github.com>
2024-08-26 17:17:41 +0200

    ex5 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a,b) {
+  if (Number.isInteger(a) && Number.isInteger(b) && a >=0 && b >= 0){
+    let res = 0;
+    let lower = Math.min(a,b);
+    let upper = Math.max(a,b);
+    for(let i=lower ; i <= upper ; i++){
+      res += i;
+    }
+    return res;
+  } 
+  return 'ERROR';
+}
 
 // Do not edit below this line
 module.exports = sumAll;
